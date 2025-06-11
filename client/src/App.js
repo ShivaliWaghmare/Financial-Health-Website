9c4d64c737b426d25d3b11fec9c2667175b8b6b5
ShivaliWaghmare <shivaliwaghmare2004@gmail.com>
2025-06-11 21:32:48 +0530

    StockPredictor Route added to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import AboutUs from './pages/AboutUs';
+import StockPredictor from './pages/StockPredictor';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/aboutUs' element={<AboutUs />} />
+        <Route path='/predictor' element={<StockPredictor />}  />
       </Routes>
     </>
   );
